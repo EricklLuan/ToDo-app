b6ee20001c9cacf04d843dc5ad01c5347e31a8be
Erick Luan <erick.luan.silva1308@gmail.com>
2023-01-02 13:33:19 -0300

    adding cards to the page by sending the form

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,29 +3,68 @@ import pencil from '../../assets/pencil-square.svg'
 
 import './style.scss'
 
-export function Card() {
+export function Card({text}) {
 
   window.onresize = () => {
-    const textarea = document.getElementById("work-text")
-    textarea.style.height = ""
-    textarea.style.height =  `${textarea.scrollHeight}px`
+    for (let index = 0; index < document.getElementsByClassName("work-text").length; index++) {
+      const element = document.getElementsByClassName("work-text")[index];
+      element.style.height = ""
+      element.style.height =  `${element.scrollHeight}px`
+    }
+  }
+
+  document.body.onload = () => {
+    for (let index = 0; index < document.getElementsByClassName("work-text").length; index++) {
+      const element = document.getElementsByClassName("work-text")[index];
+      element.style.height = ""
+      element.style.height =  `${element.scrollHeight}px`
+    }
+  }
+
+  function handleTextAreaResize(e) {
+    e.currentTarget.style.height = ""
+    e.currentTarget.style.height = `${e.currentTarget.scrollHeight}px`
+  }
+
+  function handleEditWorkText(e) {
+    const textarea = e.currentTarget.parentNode.nextSibling.childNodes[1]
+    textarea.disabled = false
+    textarea.focus()
+  }
+
+  function handleFinishEditing(e) {
+    const textarea = e.currentTarget
+    textarea.disabled = true
   }
 
   return (
-    <div className="Card">
-      <section id='menu'></section>
+    <div className="Card" onLoad={e => {
+      const textarea = e.currentTarget.childNodes[1].childNodes[1]
+      textarea.style.height = ""
+      textarea.style.height = `${textarea.scrollHeight}px`
+    }}>
+
+      <section id='menu'>
+        <button onClick={e => handleEditWorkText(e)}>
+          <img src={pencil} alt="edit card" srcset="" />
+        </button>
+        <button>
+          <img src={trash} alt="delete card" srcset="" />
+        </button>
+      </section>
+
       <section id='work'>
         <input type="checkbox" id="maked" />
         <textarea 
-          id="work-text"
+          className="work-text"
           rows={1}
-          defaultValue="condimentum mattis pellentesque id nibh tortor id aliquet lectus proin nibh nisl"
-          onInput={e => {
-            e.currentTarget.style.height = ""
-            e.currentTarget.style.height = `${e.currentTarget.scrollHeight}px`
-          }}
+          defaultValue={text}
+          onInput={e => handleTextAreaResize(e)}
+          onBlur={e => handleFinishEditing(e)}
+          disabled
         ></textarea>
       </section>
+
     </div>
   )
 }
--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 import check_square from "../../assets/check-square.svg"
 import send from "../../assets/send.svg"
 
@@ -7,8 +9,22 @@ import "./style.scss"
 
 export function App() {
 
+  const [cardText, setCardText] = useState("")
+  const [cards, setCards] = useState([])
+
   function addWork(e) {
+    e.currentTarget[0].value = ""
     e.preventDefault()
+
+    if (cardText === "") return
+
+    const newCard = {
+      id: cards.length + 1,
+      text: cardText
+    }
+    
+    setCards(prevStates => [...prevStates, newCard])
+    setCardText("")
   }
 
   return (
@@ -19,17 +35,18 @@ export function App() {
       </header>
 
       <form id="add-work" onSubmit={e => addWork(e)}>
-        <input type="text" maxLength={50} placeholder="Insira a tarefa..."/>
+        <input type="text" maxLength={50} placeholder="Insira a tarefa..." onInput={e => setCardText(e.currentTarget.value)}/>
         <button type="submit">
-          <img src={send} alt="" />
+          <img src={send} alt="send text"/>
         </button>
       </form>
 
       <div id="cards">
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+        {
+          cards.map(card => {
+            return <Card key={card.id} text={card.text} />
+          })
+        }
       </div>
     </div>
   )
